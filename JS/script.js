b2e9bd25b8f852c00a58b60940e8d004eb7e1e43
andreaconv <andrea2convertini@gmail.com>
2023-03-17 15:50:37 +0100

    end

--- JS/script.js
+++ JS/script.js
@@ -7,19 +7,34 @@ Infine scrivi sulla pagina nomecognomecolorepreferito23
 */
 
 // PASSAGGI DA EFFETTUARE
-
 /*
 1. Chiedo all'utente il suo nome;
-
 2. Chiedo il suo cognome;
-
 3. Chiedo il suo colore preferito;
-
-4. Salvo in un const i dati ricevuti
-
-5. Creo un const concatenando le stringhe
-
-6. Collego il concatenamento all'HTML
-
-7. Stampo sulla pagina il const concatenato
+4. Salvo in un const i dati ricevuti;
+5. Creo un const concatenando le stringhe;
+6. Collego il concatenamento all'HTML;
+7. Stampo sulla pagina il const concatenato;
 */
+
+const nome = prompt("Qual è il tuo nome?");
+
+const cognome = prompt("Qual è il tuo cognome?");
+
+const colore = prompt("Qual è il tuo colore preferito?");
+
+console.log(nome, cognome, colore);
+
+// CONST che addiziona i dati ricevuti
+const psw = nome + cognome + colore;
+
+console.log(psw);
+
+// CONST da collegare a HTML
+const outputStr = `
+  Ciao! La tua password generata è ${psw}23.
+`
+console.log(outputStr);
+
+// COLLEGAMENTO a HTML 
+document.getElementById("output").innerHTML = outputStr;
