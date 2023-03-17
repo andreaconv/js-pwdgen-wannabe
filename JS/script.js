1aa5fdc8e247db16934dd71c027ca56671fc7942
andreaconv <andrea2convertini@gmail.com>
2023-03-17 15:25:17 +0100

    [add] consegna e istruzioni

---
+++ JS/script.js
@@ -0,0 +1,25 @@
+// CONSEGNA
+
+/* Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito23
+*/
+
+// PASSAGGI DA EFFETTUARE
+
+/*
+1. Chiedo all'utente il suo nome;
+
+2. Chiedo il suo cognome;
+
+3. Chiedo il suo colore preferito;
+
+4. Salvo in un const i dati ricevuti
+
+5. Creo un const concatenando le stringhe
+
+6. Collego il concatenamento all'HTML
+
+7. Stampo sulla pagina il const concatenato
+*/
